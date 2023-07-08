79edcf462423c5eade573e97a10db78654b5f280
Nikolay Ignatov <ignatov.99@yandex.ru>
2023-07-08 17:05:58 +0300

    refactor code for support iOS versions <15.0

--- ThreeButtons/CustomButton.swift
+++ ThreeButtons/CustomButton.swift
@@ -10,14 +10,57 @@ import UIKit
 
 final class CustomButton: UIButton {
     
+    init(title: String) {
+        super.init(frame: .zero)
+        setTitle(title, for: .normal)
+        setImage(.init(systemName: "arrow.right.circle.fill")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        setImage(.init(systemName: "arrow.right.circle.fill")?.withRenderingMode(.alwaysTemplate), for: .highlighted)
+        
+        backgroundColor = .systemBlue
+        setTitleColor(.white, for: .normal)
+        imageView?.tintColor = .white
+        
+        layer.cornerRadius = 8
+        layer.cornerCurve = .continuous
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.beginFromCurrentState, .allowUserInteraction]) {
+                self.transform = self.isHighlighted ? .init(scaleX: 0.94, y: 0.94) : .identity
+            }
+        }
+    }
+    
     override func tintColorDidChange() {
         super.tintColorDidChange()
         if tintAdjustmentMode == .dimmed {
-            backgroundColor = .gray
-            layer.cornerRadius = 5
+            self.imageView?.tintColor = .systemGray3
+            self.setTitleColor(.systemGray3, for: .normal)
+            self.backgroundColor = .systemGray2
         } else {
-            backgroundColor = .systemBlue
-            layer.cornerRadius = 5
+            self.imageView?.tintColor = .white
+            self.setTitleColor(.white, for: .normal)
+            self.backgroundColor = .systemBlue
         }
+    }
+    
+    func setInsets() {
+        sizeToFit()
+        let imageWidth = imageView?.frame.width ?? .zero
+        let titleWidth = titleLabel?.frame.width ?? .zero
+        let titleImageSpace: CGFloat = 8
+
+        contentEdgeInsets = .init(top: 10, left: 14, bottom: 10, right: 14 + titleImageSpace)
+        
+        self.titleEdgeInsets = .init(top: 0, left: -imageWidth, bottom: 0, right: imageWidth)
+        
+        self.imageEdgeInsets = .init(top: 0, left: titleWidth + titleImageSpace, bottom: 0, right: -titleWidth - titleImageSpace)
     }
 }
--- ThreeButtons/FirstViewController.swift
+++ ThreeButtons/FirstViewController.swift
@@ -9,38 +9,21 @@ import UIKit
 
 final class FirstViewController: UIViewController {
     
-    enum ButtonType: String {
-        case first = "First Button"
-        case second = "Second Medium Button"
-        case third = "Third"
-    }
-    
-    private lazy var firstButton = CustomButton(configuration: setupButton(type: .first))
-    private lazy var secondButton = CustomButton(configuration: setupButton(type: .second))
-    private lazy var thirdButton = CustomButton(configuration: setupButton(type: .third), primaryAction: UIAction(handler: { _ in
-        self.present(SecondViewController(), animated: true)
-    }))
-    
-    private var buttonAnimator: UIViewPropertyAnimator?
+    private lazy var firstButton = CustomButton(title: "First Button")
+    private lazy var secondButton = CustomButton(title: "Second Medium Button")
+    private lazy var thirdButton = CustomButton(title: "Third")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         setupConstraints()
-        setupButtonAnimations()
     }
     
-    private func setupButton(type: ButtonType) -> UIButton.Configuration {
-        var configuration = UIButton.Configuration.filled()
-        configuration.title = type.rawValue
-        configuration.image = UIImage(systemName: "arrow.right.circle.fill")
-        configuration.baseBackgroundColor = .systemBlue
-        configuration.baseForegroundColor = .white
-        configuration.cornerStyle = .medium
-        configuration.imagePlacement = .trailing
-        configuration.imagePadding = 8
-        configuration.contentInsets = .init(top: 10, leading: 14, bottom: 10, trailing: 14)
-        return configuration
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        firstButton.setInsets()
+        secondButton.setInsets()
+        thirdButton.setInsets()
     }
     
     private func setupView() {
@@ -48,9 +31,11 @@ final class FirstViewController: UIViewController {
         view.addSubview(firstButton)
         view.addSubview(secondButton)
         view.addSubview(thirdButton)
-        firstButton.translatesAutoresizingMaskIntoConstraints = false
-        secondButton.translatesAutoresizingMaskIntoConstraints = false
-        thirdButton.translatesAutoresizingMaskIntoConstraints = false
+        thirdButton.addTarget(self, action: #selector(openModalVC), for: .touchUpInside)
+    }
+    
+    @objc private func openModalVC() {
+        self.present(SecondViewController(), animated: true)
     }
     
     private func setupConstraints() {
@@ -64,52 +49,5 @@ final class FirstViewController: UIViewController {
             thirdButton.topAnchor.constraint(equalTo: secondButton.bottomAnchor, constant: 10),
             thirdButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
-    }
-    
-    private func setupButtonAnimations() {
-        firstButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
-        firstButton.addTarget(self, action: #selector(buttonTouchUp), for: [.touchUpInside, .touchUpOutside])
-        
-        secondButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
-        secondButton.addTarget(self, action: #selector(buttonTouchUp), for: [.touchUpInside, .touchUpOutside])
-        
-        thirdButton.addTarget(self, action: #selector(buttonTouchDown), for: .touchDown)
-        thirdButton.addTarget(self, action: #selector(buttonTouchUp), for: [.touchUpInside, .touchUpOutside])
-    }
-    
-    @objc private func buttonTouchDown(_ sender: UIButton) {
-        if let buttonAnimator = buttonAnimator, buttonAnimator.isRunning {
-            buttonAnimator.pauseAnimation()
-        } else {
-            let targetScale: CGFloat = 0.9
-            buttonAnimator = UIViewPropertyAnimator(duration: 0.1, curve: .easeInOut, animations: {
-                sender.transform = CGAffineTransform(scaleX: targetScale, y: targetScale)
-            })
-            buttonAnimator?.startAnimation()
-        }
-    }
-    
-    @objc private func buttonTouchUp(_ sender: UIButton) {
-        if var buttonAnimator = buttonAnimator, buttonAnimator.isRunning {
-            buttonAnimator.pauseAnimation()
-            buttonAnimator.isReversed = true
-            let fraction = buttonAnimator.fractionComplete
-            
-            buttonAnimator.stopAnimation(true)
-            buttonAnimator.finishAnimation(at: .current)
-            
-            let targetScale: CGFloat = 1.0
-            
-            let newAnimator = UIViewPropertyAnimator(duration: 0.1, curve: .easeInOut) {
-                sender.transform = CGAffineTransform(scaleX: targetScale, y: targetScale)
-            }
-            newAnimator.fractionComplete = fraction
-            newAnimator.startAnimation()
-            buttonAnimator = newAnimator
-        } else {
-            UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.1, delay: 0, options: [], animations: {
-                sender.transform = .identity
-            })
-        }
     }
 }
